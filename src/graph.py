c8ed0438b300c593285f2d9484536fa991930d90
Jfdeev <jfrsoares161205@gmail.com>
2025-09-14 13:44:06 -0300

    feat: implements search ia agent

--- src/graph.py
+++ src/graph.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from langchain_ollama import ChatOllama
 from langgraph.graph import START, END, StateGraph
 from langgraph.types import Send
+from tavily import TavilyClient
 
 from schemas import *
 from prompts import *
@@ -16,12 +17,59 @@ reasoning_llm = ChatOllama(model="deepseek-r1:1.5b")
 # Nós
 
 def build_first_queries(state: ReportState):
+    class QueryList(BaseModel):
+        queries: List[str]
+    
     user_input = state.user_input
 
     prompt = build_queries.format(user_input=user_input)
+    query_llm = llm.with_structured_output(QueryList)
+    result = query_llm.invoke(prompt)
 
-    return {""}
+    return {"queries": result.queries}
 
+# Para rodar todos os workers de forma assincrona e paralela
+def spawn_researches(state: ReportState):
+    return [Send("single_search", query) for query in state.queries]
+
+
+def single_search(query: str):
+    tavily = TavilyClient()
+    results = tavily.search(query, max_results=3, include_raw_content=False)
+    url = results["results"][0]["url"]
+    url_etraction = tavily.extract(url)
+
+    if len(url_etraction["results"]) > 0:
+        raw_content = url_etraction["results"][0]["raw_content"]
+        prompt = resume_search.format(user_input=user_input,search_results=raw_content)
+        llm_result = llm.invoke(prompt)
+
+        query_result = QueryResult(
+            title=results["results"][0]["title"],
+            url=url,
+            resume=llm_result
+        )
+    return {"query_result": query_result}
+
+
+def final_writer(state: ReportState):
+    search_results = ""
+    references = ""
+    for i, result in enumerate(state.query_results):
+        search_results += f"[{i+1}]\n\n"
+        search_results += f"Title: {result.title}\n"
+        search_results += f"URL: {result.url}\n"
+        search_results += f"Content: {result.resume}\n\n"
+        search_results += f"---------------------------------------\n\n"
+
+        references += f"[{i+1}] - {result.title}({result.url})\n"
+
+    prompt = build_final_response.format(user_input=state.user_input, search_results=search_results)
+
+    llm_result = reasoning_llm.invoke(prompt)
+    final_response = llm_result + "\n\nReferences:\n" + references
+
+    return {"final_response": final_response}
 
 # Edges (arestas)
 builder = StateGraph(ReportState)
--- src/prompts.py
+++ src/prompts.py
@@ -19,3 +19,34 @@ that will be used to find answers to the user's question.
 
 Answer with anything between 3-5 queries.
 """
+
+
+resume_search = agent_prompt + """
+You Objective here is to analyze the web search results
+and make a synthesis of it, emphasizing only what is relevant to the user's question.
+
+After your work, another agent will use the synthesis to build a final response to the user, 
+so make a sure synthesis contains only useful information.
+Be concise and clear.
+
+Here's the web search results:
+<search_results>
+{search_results}
+</search_results>
+
+"""
+
+build_final_response = agent_prompt + """
+You objective here is devlop a final response to the user using
+the reports made during the web search, with their syntheses.
+
+The response should contain something between 500 - 800 words.
+
+Here's the web search results:
+<search_results>
+{search_results}
+</search_results>
+
+You must add reference citacions (with the number of the citation, example: [1]) for the
+articles you used in each paragraph of your answer.
+"""
